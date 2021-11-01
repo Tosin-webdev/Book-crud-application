30e19931bdc223edab948920ef4e30008ed35141
Tosin <oladejit3@gmail.com>
2021-11-01 04:51:39 -0700

    added changes

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
-let express = require('express')
-let app = express()
-const port = 3000
+let express = require("express");
+let app = express();
+const port = 3000;
 
 // A get request
-app.get('/',(req, res) =>{
-    res.render("index")
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
+app.get("/add-user", (req, res) => {
+  res.render("add-user");
+});
 
-app.get('/add-user', (req, res)=>{
-    res.render("add-user")
-})
-
-app.get('/update-user', (req, res)=>{
-    res.render("update-user")
-})
+app.get("/update-user", (req, res) => {
+  res.render("update-user");
+});
 
 //  register view engine
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
 // view public folder(css)
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-
-app.listen(port, ()=>{
-    console.log(`app listening at http://localhost:${port}`);
-})
-
+app.listen(port, () => {
+  console.log(`app listening at http://localhost:${port}`);
+});
